384fa0ca15df85a52c5ff0d416bfa7403e7cb09d
Glen Johnson <plasticarm@gmail.com>
2025-10-16 16:55:29 -0400

    Added song choice functionality
    2 songs to play while running missions.

---
+++ missions/EmpireMarch.py
@@ -0,0 +1,8 @@
+
+empireMarch = [("A4", QUATER), ("A4", QUATER), ("A4", QUATER), ("F3", EIGHTH), ("C4", EIGHTH), ("A4", QUATER), ("F3", EIGHTH), ("C4", EIGHTH), ("A4", HALF),
+                ("E4", QUATER), ("E4", QUATER), ("E4", QUATER), ("F4", EIGHTH), ("C4", EIGHTH), ("A3", QUATER), ("F3", EIGHTH), ("C4", EIGHTH), ("A4", HALF),
+                ("A4", QUATER), ("A4", QUATER), ("A4", QUATER), ("F3", EIGHTH), ("C4", EIGHTH), ("A4", QUATER), ("F3", EIGHTH), ("C4", EIGHTH), ("A4", HALF),
+                ("E4", QUATER), ("E4", QUATER), ("E4", QUATER), ("F4", EIGHTH), ("C4", EIGHTH), ("A3", QUATER), ("F3", EIGHTH), ("C4", EIGHTH), ("A4", HALF),
+                ("C5", QUATER), ("C5", QUATER), ("C5", QUATER), ("A4", EIGHTH), ("F4", EIGHTH), ("F4", DOTTED_QUATER), ("E4", EIGHTH), ("D4", EIGHTH), ("B3", HALF),
+                ("A4", QUATER), ("A4", QUATER), ("A4", QUATER), ("F3", EIGHTH), ("C4", EIGHTH), ("A4", QUATER), ("F3", EIGHTH), ("C4", EIGHTH), ("A4", HALF),
+                ("E4", QUATER), ("E4", QUATER), ("E4", QUATER), ("F4", EIGHTH), ("C4", EIGHTH), ("A3", QUATER), ("F3", EIGHTH), ("C4", EIGHTH), ("A4", HALF)]
