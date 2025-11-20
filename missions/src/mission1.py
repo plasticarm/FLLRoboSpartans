1a1a743e35a3bc95fd32c3fb71c918345fc87e2e
Glen Johnson <plasticarm@gmail.com>
2025-11-20 17:56:20 -0500

    Refactor mission logic and add fitness test script
    Replaces drive and rotate functions in mission1.py with more accurate versions, adjusts mission parameters, and comments out unused code. Adds a new comprehensive testing script (fitnessTest1.py) for robot movement and arm control. Updates common_export.py to fix a variable name in drive_straight and adds asyncio import. Renames Mission1.llsp3 to Mission1-2.llsp3 and updates related binary files.

--- missions/src/mission1.py
+++ missions/src/mission1.py
@@ -7,44 +7,48 @@ import runloop
 
 async def mission_08_silo () :
     # Drive to silo
-    await drive(40.50, 500)
+    await accurateDrive(40.50, 500)
+    for i in range(4):
+        await rotateRightArm(70, 500)
+        await rotateRightArm(-70, 500)
+    
 
 async def mission_06_forge () :
     # Drive to the 3 rocks
-    await drive(18.50, 1000)
+    await accurateDrive(16.0, 400)
     # Change direction to the right
-    await rotateDegrees(23.00, 500)
+    await accurateRotateDegrees(23.00, 500)
     # Drive closer to the rocks
-    await drive(10.00, 1000)
+    await accurateDrive(10.00, 400)
     # Hit the lever to release the rocks
-    await rotateDegrees(-20.00, 500)
+    await rotateDegrees(-10.00, 500)
     # Rotate back to the right to reposition
-    await rotateDegrees (40,500)
+    await accurateRotateDegrees (30,500)
 
 async def mission_05_who_lived_here () :
     # Reverse from the rocks
-    await drive(-15.00, 1000)
+    await accurateDrive(-12.5, 400)
     # Drive forward a little so that rotation works
-    await drive (2, 20)
+    #await drive (2, 20)
     # Rotate to the left to align with the lever
-    await rotateDegrees (-38.00, 500)
+    await accurateRotateDegrees (-38.00, 500)
     # Drive up to the lever
-    await drive (13, 500)
+    await accurateDrive (15.5, 500)
     # Hit the lever
-    await rotateDegrees (-26, 500)
+    await rotateDegrees (-24, 300)
     await rotateDegrees (6, 300)
     # Drive back away from the structure
-    await drive (-10,500)
+    await accurateDrive (-11.5,500)
 
 async def mission_10_tip_the_scales () :
     # Rotate to the left to prepare to drive to the scales
-    await rotateDegrees(-40,500)
+    await accurateRotateDegrees(-40,500)
     # Drive towards the scales
-    await drive(31, 500)
+    await accurateDrive(32, 500)
     # Rotate to the left to get in front of the scales
-    await rotateDegrees(-87,500)
+    await accurateRotateDegrees(-108,500)
     # Drive right up to the scales
-    await drive (2, 200)
+    await accurateDrive (16, 200)
     # Slowly swing arm down to hit the scales
     await rotateRightArm(70,200)
     # Bring the arm back up
@@ -52,21 +56,21 @@ async def mission_10_tip_the_scales () :
 
 async def drive_home () :
     # Back away from the scales
-    await drive (-8, 300)
+    await accurateDrive (-12, 300)
     # Rotate left
-    await rotateDegrees (-70, 500)
+    await accurateRotateDegrees (-70, 500)
     # Drive forward
-    await drive (20, 500)
+    await accurateDrive (20, 1000)
     # Rotate right
-    await rotateDegrees (40, 600)
+    await rotateDegrees (25, 600)
     #Drive towards home
     #reset arms and drive at same time.
     a = resetArmRotation()
-    b = drive (70, 500)
+    b = accurateDrive (60, 1000)
     # run the functions together
     runloop.run(*[a,b])
     # Rotate right
-    await rotateDegrees (20, 300)
+    #await accurateRotateDegrees (20, 300)
 
 async def main():
     # Starting mission: Mainmission
--- testing/fitnessTest1.py
+++ testing/fitnessTest1.py
@@ -1,18 +1,3 @@
-"""
-In SP3, you can import a text-based Python file (.py) that is present in the hub root.
-We will write a second program that exports the first program’s library functions 
-to a file called “customlib.py” in the SP root folder (“/flash”). 
-Use a slot that you don’t use for missions. We used slot 18. 
-It will first create a string that contains all the library functions. 
-Test code is excluded. It then writes the string out to a file in the SPIKE hub root. 
-It does not itself import or use the library. It is simply an exporter utility. 
-TIP:  You can also create multiple libraries by exporting chunks of code to different .py files in the root
-
-Library code string using python multiline quotes. 
-Do not include test code, only the functions you want to reuse and the imports they need.
-"""
-
-commonCode: str = """
 
 #Common functions for the SPIKE Prime robot
 
@@ -466,18 +451,59 @@ async def play_music():
         
 # --- End Async Tasks ---
 
-"""
+# Your code here
+async def fitnessTest():
+    # Example mission code. For doing multiple missions break missions into separate functions.
+    await accurateDrive(10, 500)
+    await accurateDrive(-10, 500)
+    await accurateRotateDegrees(90, 500)
+    await accurateRotateDegrees(90, 500)
+    await rotateDegrees(-90, 500)
+    await accurateDrive(10, 900)
+    await accurateRotateDegrees(180, 300)
+    await accurateRotateDegrees(-180, 300)
+    await rotateRightArm(90, 500)
+    await rotateLeftArm(90, 500)
+    await rotateRightArm(-90, 500)
+    await rotateLeftArm(-90, 500)
+    a = rotateRightArm(90, 900)
+    b = rotateLeftArm(90, 900)
+    c = accurateRotateDegrees(90, 300)
+    runloop.run(*[a, b, c])
+    a = rotateRightArm(-90, 900)
+    b = rotateLeftArm(-90, 900)
+    c = accurateRotateDegrees(-90, 300)
+    runloop.run(*[a, b, c])
+    a = rotateRightArm(90, 900)
+    b = rotateLeftArm(90, 900)
+    runloop.run(*[a, b])
+    a = rotateRightArm(-180, 900)
+    b = rotateLeftArm(-180, 900)
+    runloop.run(*[a, b])
 
-# WRITE TO SLOT 18
-def exportProgram():
-    import os
-    global code
-    os.chdir('/flash')  # change directory to root
-    try:
-        os.remove('common.py')  # remove any existing library file of the same name
-    except:
-        pass
-    with open('common.py', 'w+') as f:  # Create a new file common.py in the SPIKE hub root
-        f.write(commonCode)  # Write out the library code string to the common.py file
+    await accurateDrive(-10, 900)
+    await resetArmRotation()
+    await beep(1000, 500)
 
-exportProgram()  # Runs the export function
+async def missions():
+    # For doing multiple missions break missions into separate functions.
+    while True:
+        await fitnessTest()
+    # Add more missions as needed
+
+async def main():
+
+    # Initialize the motor pair for wheels and save motor positions. Do this every time.
+    await init()
+    # Your other code here
+    """ Optional example of running multiple tasks concurrently and playing music
+    a = button_listener()
+    b = play_music()
+    c = missions()
+    runloop.run(*[a, b, c])
+    """
+    await missions()
+    # Reset arm rotations at end so they are ready for the next run. Do this every time.
+    await resetArmRotation()
+
+runloop.run(main())
